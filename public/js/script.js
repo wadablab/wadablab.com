e66b596b95b56a70ca41aef5f66e7b10a3e4116e
wadablab <wadablab@gmail.com>
2026-03-26 09:18:21 +0100

    phone UI

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
+
     const allButtons = document.querySelectorAll('.searchBtn');
     const searchBar = document.querySelector('.searchBar');
     const searchInput = document.getElementById('searchInput');
     const searchClose = document.getElementById('searchClose');
 
     for(var i=0;i < allButtons.length; i++){
+        console.log("hello");
         allButtons[i].addEventListener('click',function(){
             searchBar.style.visibility = 'visible';
             searchBar.classList.add('open');
@@ -12,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function(){
             searchInput.focus();
         })
     }
+    
     searchClose.addEventListener('click',function(){
             searchBar.style.visibility = 'hidden';
             searchBar.classList.remove('open');
--- server/routes/main.js
+++ server/routes/main.js
@@ -77,12 +77,12 @@ router.post('/search', async(req,res) =>{
         }
 
         let searchTerm = req.body.searchTerm;
-        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g,"")
+        // const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g,"")
        
         const data = await Post.find({
             $or:[
-                {title: {$regex: new RegExp(searchNoSpecialChar,'i')}},
-                {body: {$regex: new RegExp(searchNoSpecialChar,'i')}}
+                {title: {$regex: new RegExp(searchTerm,'i')}},
+                {body: {$regex: new RegExp(searchTerm,'i')}}
             ]
         });
 
