caad38624c51eb40d8a78d60226db9895964b6bf
wadablab <wadablab@gmail.com>
2026-04-01 22:40:27 +0200

    css improvements 9

--- server/routes/main.js
+++ server/routes/main.js
@@ -12,7 +12,7 @@ router.get('',async (req,res) =>{
         title:"wadablab.com",
         description: "where worms can feel at home"
         }
-        let perPage = 5;
+        let perPage = 8;
         let page = req.query.page || 1;
 
         const data = await Post.aggregate([{$sort:{createdAt: -1}}])
